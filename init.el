ba8c40871a3e3c2dc0d3301f735f8cf833c5bed1
zbeekman <zbeekman@gmail.com>
2014-02-14 20:24:59 -0500

    Add Flymake for Emacs Lisp
    From: http://www.emacswiki.org/emacs/FlymakeElisp

--- init.el
+++ init.el
@@ -16,6 +16,40 @@
 		 (setq ispell-extra-args '("-d" "/Library/Application Support/cocoAspell/aspell6-en-6.0-0/en.multi"))
 		 ))))
 
+
+;; Flymake for ELisp
+(require 'flymake)
+(defun flymake-elisp-init ()
+  (unless (string-match "^ " (buffer-name))
+    (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                         'flymake-create-temp-inplace))
+           (local-file  (file-relative-name
+                         temp-file
+                         (file-name-directory buffer-file-name))))
+      (list
+       (expand-file-name invocation-name invocation-directory)
+       (list
+        "-Q" "--batch" "--eval" 
+        (prin1-to-string
+         (quote
+          (dolist (file command-line-args-left)
+            (with-temp-buffer
+              (insert-file-contents file)
+              (condition-case data
+                  (scan-sexps (point-min) (point-max))
+                (scan-error
+                 (goto-char(nth 2 data))
+                 (princ (format "%s:%s: error: Unmatched bracket or quote\n"
+                                file (line-number-at-pos)))))))
+          )
+         )
+        local-file)))))
+(push '("\\.el$" flymake-elisp-init) flymake-allowed-file-name-masks)
+(add-hook 'emacs-lisp-mode-hook
+          ;; workaround for (eq buffer-file-name nil)
+          (function (lambda () (if buffer-file-name (flymake-mode)))))
+
+
 ;; Save the emacs buffer state to local directory
 (desktop-save-mode 1)
 
